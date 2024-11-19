73572b1be11dbaf4bb9e557100edef031cacf635
Julian Wilkison-Duran <clockworksyler@gmail.com>
2024-11-19 05:50:54 -0500

    add comments

--- backend/database.go
+++ backend/database.go
@@ -7,6 +7,7 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
+// Database connection variable.
 var db *sql.DB
 
 func connectDatabase() {
@@ -24,9 +25,11 @@ func saveInteraction(query, response string) {
 	}
 }
 
+// logInteraction logs user interactions including their queries, responses, and ratings into the database.
 func logInteraction(query, response string, rating int) {
+	// Store user query, response, and rating in the interaction_logs table
 	_, err := db.Exec("INSERT INTO interaction_logs(query, response, feedback_rating) VALUES(?, ?, ?)", query, response, rating)
 	if err != nil {
-		log.Println("Error logging interaction:", err)
+		log.Println("Error logging interaction:", err) // Log any error encountered while logging interaction
 	}
 }
